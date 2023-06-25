fad183ec04c9aac593d5c9cc04dab28a18db7ca2
MhannaCodeMaster <anthonymhanna8@gmail.com>
2023-06-25 22:38:58 +0300

    fixed export bugs

--- app.js
+++ app.js
@@ -67,6 +67,6 @@ app.use((req, res, next) => {
   res.status(404).render('error404', { pageTitle: 'Page Not Found' });
 });
 
-app.listen(5001, () => {
-  console.log('Server listening on port 5001');
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
 });
